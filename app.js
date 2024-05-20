e227992fec26bd7fb375f96058a4960cfa54d765
Rimsha E <111149954+rimshaejaz@users.noreply.github.com>
2024-05-20 16:15:02 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://ejazrimsha:Friend101@cluster0.rvuvigq.mongodb.net/', () => {
+    console.log("DB connected.");
+});
+
+
+function generatePassword() {
+  let password = '';
+  let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+      'abcdefghijklmnopqrstuvwxyz' + '0123456789' + '!%&@#$';
+
+  for (let i = 1; i <= 10; i++) {
+      let char = Math.floor(Math.random()
+          * str.length + 1);
+
+      password += str.charAt(char)
+  }
+
+  return password;
+};
+
+// create password 
+app.post('/', (req, res) => {
+  res.send(generatePassword())
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+
+
